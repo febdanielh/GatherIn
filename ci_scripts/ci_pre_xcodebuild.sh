4a187b03ba3a8c457c3d1b1abd5c2818e6dff6cc
febdanielh <febriandanielh09@gmail.com>
2024-05-13 17:07:55 +0700

    update scripts
    update to build xcc

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,24 +3,26 @@
 echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
-# This is important because the position of the subsequently mentioned files depend of this origin.
+# This is important because the position of the subsequently mentioned files depends on this origin.
 cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 # Create the plist content
-SECRET_CONTENT= "
+SECRET_CONTENT=$(cat <<EOF
 import Foundation
 
 enum SupaExt {
-    static var url = URL(string: \"$SUPABASE_URL\")!
-    static var key = \"$SUPABASE_API_KEY\"
+    static var url = URL(string: "$SUPABASE_URL")!
+    static var key = "$SUPABASE_API_KEY"
 }
-"
+EOF
+)
 
-# Write a JSON File containing all the environment variables and secrets.
-echo "${SECRET_CONTENT}" > ../Sportify/Constant/SupaExt.swift
+# Write a Swift file containing the secret content.
+echo "$SECRET_CONTENT" > ../Sportify/Constant/SupaExt.swift
 
-echo "Wrote Secrets.plist file."
+echo "Wrote SupaExt.swift file."
 
 echo "Stage: PRE-Xcode Build is DONE .... "
 
 exit 0
+
