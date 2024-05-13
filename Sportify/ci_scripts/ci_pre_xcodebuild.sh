f0a3724bf03c757f9e1c8141c1a38c9d6ee6cbfe
febdanielh <febriandanielh09@gmail.com>
2024-05-13 16:31:59 +0700

    setup ci
    setup ci secret content

---
+++ Sportify/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Stage: PRE-Xcode Build is activated .... "
+
+# Move to the place where the scripts are located.
+# This is important because the position of the subsequently mentioned files depend of this origin.
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
+
+# Create the plist content
+SECRET_CONTENT= "import Foundation
+
+enum SupaExt {
+    static var url = URL(string: \"$SUPABASE_URL\")!
+    static var key = \"$SUPABASE_API_KEY\"
+}"
+
+# Write a JSON File containing all the environment variables and secrets.
+echo "${SECRET_CONTENT}" > ../Constant/SupaExt.swift
+
+echo "Wrote Secrets.plist file."
+
+echo "Stage: PRE-Xcode Build is DONE .... "
+
+exit 0
